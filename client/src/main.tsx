a49c19bd47ac3861c7f975e5815544d063c1457e
Nathan louissaint <louissaintmark@gmail.com>
2026-07-31 13:42:28 -0500

    feat(ui): add global toast notification provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "sonner";
+
 import App from "./App";
 import { AuthProvider } from "./auth/AuthProvider";
 import { queryClient } from "./lib/queryClient";
+
 import "./index.css";
 
 ReactDOM.createRoot(
@@ -14,6 +17,14 @@ ReactDOM.createRoot(
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <App />
+
+        <Toaster
+          position="top-right"
+          richColors
+          closeButton
+          expand={false}
+          duration={4000}
+        />
       </AuthProvider>
 
       <ReactQueryDevtools
